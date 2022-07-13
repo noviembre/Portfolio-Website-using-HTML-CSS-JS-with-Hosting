1ced4b4526aacc0644da9a87406f598bd1a05e18
Manu Linux <perudragon@hotmail.com>
2022-07-13 07:34:56 -0500

    02 avi. until 3.33min

---
+++ script.js
@@ -0,0 +1,14 @@
+// Mouse Circle
+const mouseCircle = document.querySelector(".mouse-circle");
+const mouseDot = document.querySelector(".mouse-dot");
+
+
+// let mouseCircleBool = true;
+
+// const mouseCircleFn = (x, y) => {
+//     mouseCircleBool &&
+//     (mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`);
+//
+//     mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
+// };
+// End of Mouse Circle
