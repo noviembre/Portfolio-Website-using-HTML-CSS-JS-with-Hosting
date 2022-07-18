e472fc154bd7ebd4aee2e4d6b8215c12b614015f
Manu Linux <perudragon@hotmail.com>
2022-07-18 09:23:07 -0500

    Animated circles 1
    video 4. win 5.04

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // Mouse Circle
+
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
-
-
-// Mouse Circle
 
 const mouseCircleFn = (x, y) => {
 
@@ -29,3 +27,39 @@ document.body.addEventListener("mouseleave", () => {
 
 
 // End of Mouse Circle
+
+// Animated Circles
+const circles = document.querySelectorAll(".circle");
+const mainImg = document.querySelector(".main-circle img");
+
+let mX = 0;
+let mY = 0;
+
+
+const animateCircles = (e, x, y) => {
+    if (x < mX) {
+        console.log('move to the left');
+    }
+    if (y < mX) {
+        console.log('move upwards');
+    }
+
+    mX = e.clientX;
+    mY = e.clientY;
+
+
+};
+// End of Animated Circles
+
+document.body.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    mouseCircleFn(x, y);
+    animateCircles(e, x, y)
+});
+
+document.body.addEventListener("mouseleave", () => {
+    mouseCircle.style.opacity = "0";
+    mouseDot.style.opacity = "0";
+});
